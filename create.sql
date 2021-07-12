781d0e4b351e70a0791e1e1bdb51d2a77723a4d2
weslley lima <weslleyclima@yahoo.com.br>
2021-07-12 11:31:26 -0300

    Incluir arquivo sql

---
+++ create.sql
@@ -0,0 +1,2 @@
+create sequence hibernate_sequence start 1 increment 1
+create table sala_de_reuniao (id int8 not null, data varchar(255), fim_do_horaio varchar(255), inicio_do_horario varchar(255), nome varchar(255), primary key (id))
